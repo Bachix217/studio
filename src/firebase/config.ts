478644c3f7ad92f73191067d3bde42d8500344f2
jeremy Bächtold <jeremy.bachi10@gmail.com>
2025-12-27 13:40:55 +0000

    voici le bucket : gs://tacotoch.firebasestorage.app/vehicles

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBMQncBx6WQquE08yDIa5YxnUMAFpyyOj4",
   authDomain: "tacotoch.firebaseapp.com",
   projectId: "tacotoch",
-  storageBucket: "tacotoch.appspot.com",
+  storageBucket: "tacotoch.firebasestorage.app",
   messagingSenderId: "906277073490",
   appId: "1:906277073490:web:3050f2d6134c224d27a58b"
 };
